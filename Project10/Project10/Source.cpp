967983a59792b8290812235597dcd0dba1386de5
Obidov040102 <91002341+Obidov040102@users.noreply.github.com>
2021-09-20 09:13:58 +1300

    Add files via upload

---
+++ Project10/Project10/Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, max, min;
+	cin >> a >> b;
+	max = (a + b + abs(a - b)) / 2;
+	min = (a + b - abs(a - b)) / 2;
+	cout << "max" << max << endl;
+	return 0;
+}
