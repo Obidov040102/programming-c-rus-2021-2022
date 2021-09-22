17814d079fcf3f887e6139f218064451dcfa77f1
Obidov040102 <91002341+Obidov040102@users.noreply.github.com>
2021-09-23 02:58:44 +1300

    Add files via upload

---
+++ 2021.09.19. Homework -1/Project8/Source8.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 0;
+	int b = 0; 
+	int c = 0;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	cin >> a >> b >> c >> x >> y >> z;
+	cout << (x - a) * 3600 + (y - b) * 60 + (x - c) << endl;
+	return 0;
+}
