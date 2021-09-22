0236fbebce77f2f63b6496d9ce8ab332462cb8bb
Obidov040102 <91002341+Obidov040102@users.noreply.github.com>
2021-09-23 02:56:09 +1300

    Add files via upload

---
+++ 2021.09.19. Homework -1/Project1/Project1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(int agre, char* argv[])
+{
+	int n = 0;
+	cin >> n;
+	cout << "The next number for the number " << n << " is " << n + 1 << endl;
+	cout << "The next number for the number " << n << " is " << n - 1 << endl;
+	return 0;
+}
