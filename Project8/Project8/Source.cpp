0a1959a1e38d269082666d55058bb623ff16ffd3
Obidov040102 <91002341+Obidov040102@users.noreply.github.com>
2021-09-22 23:05:17 +1300

    Delete Project8 directory

--- Project8/Project8/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int a, b, c, x, y, z;
-	cin >> a >> b >> c >> x >> y >> z;
-	cout << (x - a) * 3600 + (y - b) * 60 + (x - c) << endl;
-	return 0;
-}
