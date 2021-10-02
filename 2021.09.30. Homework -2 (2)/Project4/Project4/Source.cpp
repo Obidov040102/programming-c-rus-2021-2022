c3b886acd49b0283d87d1a64f65d15852133852d
Obidov040102 <91002341+Obidov040102@users.noreply.github.com>
2021-10-02 23:20:40 +0300

    Add files via upload

---
+++ 2021.09.30. Homework -2 (2)/Project4/Project4/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int v = 0;
+	int  t = 0;
+	int S = 109;
+	cin >> v >> t;
+	cout << endl;
+	cout << " " << (v * t % 109 + 109) % 109 << endl;
+	return 0;
+}
