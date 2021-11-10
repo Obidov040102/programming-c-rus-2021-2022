d672a952a4a0474c92e656e6ef4dae6c7518691d
Obidov040102 <91002341+Obidov040102@users.noreply.github.com>
2021-11-10 20:48:18 +0300

    Add files via upload

---
+++ 2021.11.08 Homework - 5/Poject1/Исходный код.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int n = 0;
+	int a = 0; // первый сменый бит
+	int b = 0; // второй сменый бит
+
+	cin >> n >> a >> b;
+
+	int abit = (n & (1 << a)) >> a;
+	n = n & (~(1 << a));
+	int bbit = (n & (1 << a)) >> a;
+	n = n & (~(1 << b));
+	n = n | (abit << b) | (bbit << a);
+	cout << n << endl;
+	return EXIT_SUCCESS;
+}
